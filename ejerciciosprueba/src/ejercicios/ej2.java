a36b357c37dc9bc84e20453ee8948f75bd6ee070
ivan524306vueling <ivan524306@gmail.com>
2018-10-02 22:24:32 +0200

    commit inicial

---
+++ ejerciciosprueba/src/ejercicios/ej2.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class ej2 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World2");
+    }
+
+}
